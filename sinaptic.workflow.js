c5fe551a9ef71bb9c1446d72c42e51d4ad149860
Lucas Siri <l.siri07@vemn.com.ar>
2017-08-27 16:08:47 -0300

    nuevo test collapse bootstrap

--- sinaptic.workflow.js
+++ sinaptic.workflow.js
@@ -343,14 +343,13 @@ sinaptic.wf = function () {
                 taskContent.push("<button type='button' data-toggle='collapse' data-target='#comentariosContainer' class='btn btn-warning'>Crear comentario");
 				taskContent.push("</div>");
 				  
-				taskContent.push("<div class='col-md-8'>");
-                taskContent.push("<div id='comentariosContainer' class='collapse' style='display:none;'>");
+			
+                taskContent.push("<div id='comentariosContainer' class='collapse'>");
                 taskContent.push("<label class='control-label'>Comentario</label>");
                 taskContent.push("<textarea id='comentario' class='form-control'>");
                 taskContent.push("</textarea>");
 				taskContent.push("</div>");
-				taskContent.push("</div>");
-
+		
                 taskContent.push("</div>");
 
                 taskContent.push(" <div class='form-group'>");
