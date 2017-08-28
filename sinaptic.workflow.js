b56215b6e9c03f94291632ca3a95cede2844451f
Lucas Siri <l.siri07@vemn.com.ar>
2017-08-27 21:30:07 -0300

    mejoras

--- sinaptic.workflow.js
+++ sinaptic.workflow.js
@@ -96,14 +96,14 @@ sinaptic.wf = function () {
                 $(sinaptic.vm.willisusers).each(function (i, user) {
                     if (user.Grupo.Identificador === 4) {
                         responsables.push("<option value='");
-                        responsables.push(user.Cuenta);
+                        responsables.push(user.Usuario.Cuenta);
                         responsables.push("'>");
                         responsables.push(user.Usuario.Nombre);
                         responsables.push("</option>");
                     }
                     else {
                         teamleaders.push("<option value='");
-                        teamleaders.push(user.Cuenta);
+                        teamleaders.push(user.Usuario.Cuenta);
                         teamleaders.push("'>");
                         teamleaders.push(user.Usuario.Nombre);
                         teamleaders.push("</option>");
