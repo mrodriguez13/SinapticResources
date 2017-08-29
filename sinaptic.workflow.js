86ab7fa3a5ead19d3034b94fe31ae6c17004df3d
Lucas Siri <l.siri07@vemn.com.ar>
2017-08-29 17:00:31 -0300

    fix

--- sinaptic.workflow.js
+++ sinaptic.workflow.js
@@ -488,16 +488,16 @@ sinaptic.wf = function () {
                 addRemoveLinks: true,
                 dictDefaultMessage: dropZoneMessage,
                 dictRemoveFile: "Quitar",
-                dictMaxFilesExceeded: "No puede subir mas documentos",
-                init: function () {
-                    var submitButton = document.querySelector(".modal-footer>.btn .btn-success");
-                    var myDropzone = this;
-                    //submitButton.addEventListener("click", function () {
-                    //    myDropzone.processQueue(true);
-                        }
-                    });
-                }
-            //});
+                dictMaxFilesExceeded: "No puede subir mas documentos"
+                //init: function () {
+                //    var submitButton = document.querySelector(".modal-footer>.btn .btn-success");
+                //    var myDropzone = this;
+                //    submitButton.addEventListener("click", function () {
+                //        myDropzone.processQueue(true);
+                        
+                //    });
+                //}
+            });
         }
         $("#modaltask").modal();
     };
