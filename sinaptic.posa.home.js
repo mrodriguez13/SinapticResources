e7abed60e5a1a877638ac1090ca8ae52c45be54e
CristianSinaptic <cdominguez@sinaptic.com.ar>
2017-09-05 23:45:48 -0300

    fix tareas vencidas por estado chart

--- sinaptic.posa.home.js
+++ sinaptic.posa.home.js
@@ -387,7 +387,7 @@ sinaptic.posa = function (options) {
             element: "overdueTasksContainer",
             // Chart data records -- each entry in this array corresponds to a point on
             // the chart.
-            data: dataAging,
+            data: dataOverdue,
             // The name of the data record attribute that contains x-values.
             xkey: "estado",
             // A list of names of data record attributes that contain y-values.
