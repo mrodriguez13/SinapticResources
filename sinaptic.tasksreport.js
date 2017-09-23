891a9a8083c2d6b8e06258803bb1f668211ba46a
lsiri <31389775+lsiri@users.noreply.github.com>
2017-09-23 17:58:48 -0300

    Add files via upload

--- sinaptic.tasksreport.js
+++ sinaptic.tasksreport.js
@@ -1,15 +1,29 @@
 ﻿
 $(document).ready(function () {
-
-
+    
+    getFromHistory();
+    buildDatatask();
 });
 
-function currentUserData() {
-   
-}
+
 
 function getFromHistory() {
+    var usersUrl = settings.host + "/_vti_bin/listdata.svc/Historial";
 
+    $.ajax({
+        url: usersUrl,
+        type: "GET",
+        async: true,
+        headers: {
+            "accept": "application/json;odata=verbose"
+        },
+        success: function (data) {
+
+            console.log(data.d.results);
+
+        },
+        error: errorHandler
+    });
 }
 
 function buildDatatask() {
