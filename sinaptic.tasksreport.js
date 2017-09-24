44614d54d3d35858930d8edfdbf9e26d4a00e39d
lsiri <31389775+lsiri@users.noreply.github.com>
2017-09-23 21:13:21 -0300

    Add files via upload

--- sinaptic.tasksreport.js
+++ sinaptic.tasksreport.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
 
 
 function getFromHistory() {
-    var usersUrl = host + "/_vti_bin/listdata.svc/Historial?$expand=Siniestro,Estado&$select=*,Siniestro/Siniestro,Estado";
+    var usersUrl = host + "/_vti_bin/listdata.svc/Historial?$expand=Siniestro,Estado&$select=*,Siniestro/Siniestro";
 
     $.ajax({
         url: usersUrl,
