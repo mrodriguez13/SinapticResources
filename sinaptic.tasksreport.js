7f71ffa922afba380eb2c2c07b23cd2b069cacc7
lsiri <31389775+lsiri@users.noreply.github.com>
2017-09-23 22:03:24 -0300

    Add files via upload

--- sinaptic.tasksreport.js
+++ sinaptic.tasksreport.js
@@ -34,10 +34,29 @@ function getFromHistory() {
             for (var i = 0; i < result; i++) {
                 sinisterData.Siniestro = (data.d.results[i].Siniestro != null) ? data.d.results[i].Siniestro.Siniestro : "";
                 sinisterData.Estado = data.d.results[i].Estado.Descripción;
-                sinisterData.FechaDesde = data.d.results[i].FechaDesde;
-                sinisterData.FechaHasta = data.d.results[i].FechaHasta;
+
+                var fechaDesde = data.d.results[i].FechaDesde;
+                fechaDesde = fechaDesde.replace("/Date(", "");
+                fechaDesde = fechaDesde.replace(")/", "");
+
+                var date_Desde = new Date(fechaDesde);
+                var diaDesde = date_Desde.getDay();
+                var mesDesde = date_Desde.getMonth() + 1;
+                var añoDesde = date_Desde.getYear();
+
+                sinisterData.FechaDesde = diaDesde + "/" + mesDesde + "/" + añoDesde;
 
 
+                var fechaHasta = data.d.results[i].FechaHasta;
+                fechaHasta = fechaHasta.replace("/Date(", "");
+                fechaHasta = fechaHasta.replace(")/", "");
+
+                var date_Hasta = new Date(fechaHasta);
+                var diaHasta = date.getDay();
+                var mesHasta = date.getMonth() + 1;
+                var añoHasta = date.getYear();
+                sinisterData.FechaHasta = diaHasta + "/" + mesHasta + "/" + añoHasta;
+ 
                 if (sinisterData.FechaHasta != null && sinisterData.Siniestro != "") {
                     $("#reportBody").append("<tr><td>" + sinisterData.Siniestro + "</td><td>" + sinisterData.Estado + "</td><td>" + sinisterData.FechaDesde + "</td><td>" + sinisterData.FechaHasta + "</td></tr>");
                 }
