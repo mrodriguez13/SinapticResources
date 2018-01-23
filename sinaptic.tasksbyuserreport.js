39c874581d7d9233ab34a243139e0cdf4c0e2df0
U586580 <cdominguez@proveedor.telecom.com.ar>
2018-01-23 02:57:20 -0300

    fix

--- sinaptic.tasksbyuserreport.js
+++ sinaptic.tasksbyuserreport.js
@@ -52,7 +52,7 @@ sinaptic.tasksByUserReport = function () {
         var filterByUser = "";
         if (userId !== 0)
             filterByUser = '(ModificadoPorId eq " + userId + ") and ';
-        var reportUrl = settings.host + "/_vti_bin/listdata.svc/Historial?$filter=" + filterByUser + "FechaHasta ne null&$expand=Siniestro,Estado";
+        var reportUrl = settings.host + "/_vti_bin/listdata.svc/Historial?$filter=" + filterByUser + "FechaHasta ne null and Siniestro ne null&$expand=Siniestro,Estado";
         $.ajax({
             url: reportUrl,
             type: "GET",
