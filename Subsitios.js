150294c283bc1981df32f148e9976df7a449eb27
Diego Bevilacqua <diegofbevilacqua1998@gmail.com>
2019-03-05 20:14:05 -0300

    Update Subsitios.js

--- Subsitios.js
+++ Subsitios.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
 	       
 	    var element = '<span><h2 class="text-title" style="color: white;text-align:left;margin:0px">' + value.LinkTitle + '</h2></span><span><p class="text-description" style="color: white;text-align:left;margin:0px">Descripción de la sección "Gestiones personales".<p></span><br>'; 
 	       	
-	    $("#tituloYDescripcion").append(element);	          
+	    $("#tituloYDescripcion").html(element);	          
        });      
        		
     });
