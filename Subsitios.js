13b7adfa10334d878c4432fb8dfa4ee02da70034
Diego Bevilacqua <diegofbevilacqua1998@gmail.com>
2019-03-06 12:59:58 -0300

    Update Subsitios.js

--- Subsitios.js
+++ Subsitios.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
     $pnp.sp.site.rootWeb.lists.getByTitle("MenuPrincipal").items.get().then(r => {
        $.each(r, function (index, value) { 
 	       
-	    var element = '<span><h2 class="text-title" style="color: white;text-align:left;margin:0px">' + value.LinkTitle + '</h2></span><span><p class="text-description" style="color: white;text-align:left;margin:0px">Descripción de la sección "Gestiones personales".<p></span><br>'; 
+	    var element = '<span><h2 class="text-title" style="color: white;text-align:left;margin:0px">' + value.Title + '</h2></span><span><p class="text-description" style="color: white;text-align:left;margin:0px">' + value.Descripcion + '<p></span><br>'; 
 	       	
 	    $("#tituloYDescripcion").html(element);	          
        });      
