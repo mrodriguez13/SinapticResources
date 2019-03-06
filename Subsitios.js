1c68c49f60a1c5b7a15c418fb0ed023fabd17b7a
Diego Bevilacqua <diegofbevilacqua1998@gmail.com>
2019-03-06 14:57:50 -0300

    Update Subsitios.js

--- Subsitios.js
+++ Subsitios.js
@@ -11,9 +11,11 @@ $(document).ready(function() {
     });
 
     $pnp.sp.site.rootWeb.lists.getByTitle("MenuPrincipal").items.get().then(r => {
-       $.each(r, function (index, value) {	    	
-		var element = '<span><h2 class="text-title" style="color: white;text-align:left;margin:0px">' + value.Title + '</h2></span><span><p class="text-description" style="color: white;text-align:left;margin:0px">' + value._x0066_hb3 + '<p></span><br>'; 
-	        $("#tituloYDescripcion").html(element);	 
+       $.each(r, function (index, value) {	
+	       if(value.fo9i == window.location.href) {
+		    var element = '<span><h2 class="text-title" style="color: white;text-align:left;margin:0px">' + value.Title + '</h2></span><span><p class="text-description" style="color: white;text-align:left;margin:0px">' + value._x0066_hb3 + '<p></span><br>'; 
+	            $("#tituloYDescripcion").html(element);	 
+	       }
        });      
        		
     });
