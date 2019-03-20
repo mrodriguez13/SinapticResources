08fdf5334345f2e40ededca78bce190c8f508882
Diego Bevilacqua <diegofbevilacqua1998@gmail.com>
2019-03-20 16:44:31 -0300

    Update Documentos.js

--- Documentos.js
+++ Documentos.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
        $.each(r, function (index, value) {
        
             var documento = '<div class="col-md-3 app-access"><a href="' + value.URL + '" class="panel panel-default panel-icon panel-primary"><div class="panel-heading hidden-xs"><div class="left-panel"><img src="' + value.Icono + '"/></div><div class="rigth-panel"><span class="visible-xs-inline">' + value.Title + '</span></div></div></a></div>';
-            $("#documentos").html(documento);   
+            $("#documentos").append(documento);   
                 
         });
 
