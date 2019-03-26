664146d8037c76a441fc736c556abdd935b23bca
Diego Bevilacqua <diegofbevilacqua1998@gmail.com>
2019-03-25 21:37:58 -0300

    Update Documentos.js

--- Documentos.js
+++ Documentos.js
@@ -1,4 +1,19 @@
-var documentos = [];
+var filas = 0;
+var documentos = 0;
+
+function agregarDocumento(documento) {
+
+    if (documentos % 3 != 0) {
+        $("#fila"+filas).append(documento);
+        documentos++;
+    }
+    else {
+        var fila = '<div id="fila' + filas + '" class="row"></div>';
+        $("#documentos").append(fila);
+        $("#fila"+filas).append(documento);
+        filas++;
+    }
+}
 
 $(document).ready(function() {	
 
@@ -9,19 +24,17 @@ $(document).ready(function() {
             }
         }
     });
-    
-			
-       $pnp.sp.site.rootWeb.lists.getByTitle("AccesosDocumentos").items.get().then(r => {
+    			
+    $pnp.sp.site.rootWeb.lists.getByTitle("AccesosDocumentos").items.get().then(r => {
 
        $.each(r, function (index, value) {
-       
+
             var documento = '<div class="col-md-4 app-access"><img src="' + value.Icono.Url + '"; width=35px;/><a href="' + value.Documento + '"><span class="visible-xs-inline" style="margin-left: 12px">' + value.Title + '</span></a></div>';
-            $("#documentos").append(documento);   
+            agregarDocumento(documento);
                 
         });
 
     });
-		
-		
-    });
+			
+});
     
