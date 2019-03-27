e328c97a46db456a8bd2a43444b8bc0f79a6919a
Diego Bevilacqua <diegofbevilacqua1998@gmail.com>
2019-03-27 16:25:34 -0300

    Update Documentos.js

--- Documentos.js
+++ Documentos.js
@@ -36,7 +36,7 @@ $(document).ready(function() {
             }
             */
             
-            var documento = '<div class="col-md-3" app-access"><img src="' + value.Icono.Url + '"; width=35px;/><a href="' + value.Documento + '"><span class="visible-xs-inline" style="margin-left: 12px">' + value.Title + '</span></a></div>';
+            var documento = '<div class="col" app-access"><img src="' + value.Icono.Url + '"; width=35px;/><a href="' + value.Documento + '"><span class="visible-xs-inline" style="margin-left: 12px">' + value.Title + '</span></a></div>';
             $("#documentos").append(documento);
 		});  	       
     });
