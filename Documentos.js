d25ed9c0cfcb330e1bedd4c289f9417bda7fdccb
Diego Bevilacqua <diegofbevilacqua1998@gmail.com>
2019-03-27 16:17:16 -0300

    Update Documentos.js

--- Documentos.js
+++ Documentos.js
@@ -14,11 +14,11 @@ $(document).ready(function() {
     $pnp.sp.site.rootWeb.lists.getByTitle("AccesosDocumentos").items.get().then(r => {
 
         $.each(r, function (index, value) {
-	       
+           
+            /*
 	        if (window.location.href == value.Subsitio && documentos < 5) {
-	            //documentos++;
+	            documentos++;
 	      
-			/*
 	            switch (documentos) {
 		            case 1: 
 			            grid = "col-md-12";
@@ -33,11 +33,11 @@ $(document).ready(function() {
 			            grid = "col-md-3";
                         break;
                 }
-		*/
             }
+            */
             
             var documento = '<div class="col-md-4" app-access"><img src="' + value.Icono.Url + '"; width=35px;/><a href="' + value.Documento + '"><span class="visible-xs-inline" style="margin-left: 12px">' + value.Title + '</span></a></div>';
-            $("#documentos").append(documento);
-	});  	       
+
+		});  	       
     });
 })
