d04f1f489d891cc1e911993902069a4d45b03989
Diego Bevilacqua <diegofbevilacqua1998@gmail.com>
2019-03-27 15:45:16 -0300

    Update Documentos.js

--- Documentos.js
+++ Documentos.js
@@ -1,20 +1,5 @@
-var filas = 0;
 var documentos = 0;
-
-function agregarDocumento(documento) {
-
-    if (documentos % 3 != 0) {
-        $("#fila"+filas).append(documento);
-        documentos++;
-    }
-    else {
-	filas++;
-        var fila = '<div id="fila' + filas + '" class="row"></div>';
-        $("#documentos").append(fila);
-        $("#fila"+filas).append(documento);
-	documentos++;
-    }
-}
+var grid;
 
 $(document).ready(function() {	
 
@@ -31,8 +16,24 @@ $(document).ready(function() {
        $.each(r, function (index, value) {
 	       
 	   if (window.location.href == value.Subsitio) {
-              var documento = '<div class="col-md-4 app-access"><img src="' + value.Icono.Url + '"; width=35px;/><a href="' + value.Documento + '"><span class="visible-xs-inline" style="margin-left: 12px">' + value.Title + '</span></a></div>';
-              agregarDocumento(documento);
+	      documentos++;
+	      
+	      switch (documentos) {
+		      case 1: 
+			      grid = "col-md-12";
+			      break;
+		      case 2: 
+			      grid = "col-md-6";
+			      break;
+	              case 1: 
+			      grid = "col-md-4";
+			      break;
+		      case 1: 
+			      grid = "col-md-3";
+			      break;
+		   
+              var documento = '<div class="' + grid + '" app-access"><img src="' + value.Icono.Url + '"; width=35px;/><a href="' + value.Documento + '"><span class="visible-xs-inline" style="margin-left: 12px">' + value.Title + '</span></a></div>';
+		
 	   }  
 	       
         });
