7bfab094d0f68a81b06b7cc32c6a40a0824eb974
Diego Bevilacqua <diegofbevilacqua1998@gmail.com>
2019-03-27 15:51:22 -0300

    Update Documentos.js

--- Documentos.js
+++ Documentos.js
@@ -13,32 +13,29 @@ $(document).ready(function() {
     			
     $pnp.sp.site.rootWeb.lists.getByTitle("AccesosDocumentos").items.get().then(r => {
 
-       $.each(r, function (index, value) {
+        $.each(r, function (index, value) {
 	       
-	   if (window.location.href == value.Subsitio) {
-	      documentos++;
+	        if (window.location.href == value.Subsitio && documentos < 5) {
+	            documentos++;
 	      
-	      switch (documentos) {
-		      case 1: 
-			      grid = "col-md-12";
-			      break;
-		      case 2: 
-			      grid = "col-md-6";
-			      break;
-	              case 1: 
-			      grid = "col-md-4";
-			      break;
-		      case 1: 
-			      grid = "col-md-3";
-			      break;
-		   
-              var documento = '<div class="' + grid + '" app-access"><img src="' + value.Icono.Url + '"; width=35px;/><a href="' + value.Documento + '"><span class="visible-xs-inline" style="margin-left: 12px">' + value.Title + '</span></a></div>';
-		
-	   }  
-	       
-        });
+	            switch (documentos) {
+		            case 1: 
+			            grid = "col-md-12";
+			            break;
+		            case 2: 
+			            grid = "col-md-6";
+			            break;
+	                case 3: 
+			            grid = "col-md-4";
+			            break;
+		            case 4: 
+			            grid = "col-md-3";
+                        break;
+                }
+            }
+            
+            var documento = '<div class="' + grid + '" app-access"><img src="' + value.Icono.Url + '"; width=35px;/><a href="' + value.Documento + '"><span class="visible-xs-inline" style="margin-left: 12px">' + value.Title + '</span></a></div>';
 
+		});  	       
     });
-			
-});
-    
+})
