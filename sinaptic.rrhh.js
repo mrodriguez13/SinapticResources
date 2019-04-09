6a0033e6e87e70c4c2904ee929464aec945c2bd6
Diego Bevilacqua <diegofbevilacqua1998@gmail.com>
2019-04-09 18:57:47 -0300

    Update sinaptic.rrhh.js

--- sinaptic.rrhh.js
+++ sinaptic.rrhh.js
@@ -9,11 +9,13 @@ $(document).ready(function() {
     });
     
     $pnp.sp.web.lists.getByTitle("Tilesrrhh").items.get().then(r => {
-        $.each(r, function(index, value) {                      
+        $.each(r, function(index, value) {   
+          if (value.ParentID != null) {
             if(value.ParentID.get_lookupValue() == 'Presentismo') {
                 var boton = '<button class="boton">' + value.Title + '</button>';
                 $(".botones").append(boton);
             }
+          }    
         });                                                              
     });
 
