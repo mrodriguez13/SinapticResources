4394a9ab26c6803e30ec77d1ab116eefdeaeb937
Diego Bevilacqua <diegofbevilacqua1998@gmail.com>
2019-04-10 00:43:19 -0300

    Update sinaptic.rrhh.js

--- sinaptic.rrhh.js
+++ sinaptic.rrhh.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
     $pnp.sp.web.lists.getByTitle("Tilesrrhh").items.get().then(r => {
         $.each(r, function(index, value) {          
             
-            var boton = '<div class="panel__inner" onclick="window.location = ' + value.URL + '">' + value.Title + '</a>'
+            var boton = '<div class="panel__inner" onclick="window.location = \'' + value.URL + '\'">' + value.Title + '</a>'
             
             if (value.Parent == 'Presentismo') {
                 $(".mosaico").append(boton);
