36140de3d68e1e6d1d7cd42226932093962b5f50
Diego Bevilacqua <diegofbevilacqua1998@gmail.com>
2019-04-11 23:50:19 -0300

    Update sinaptic.rrhh.js

--- sinaptic.rrhh.js
+++ sinaptic.rrhh.js
@@ -22,6 +22,13 @@ $(document).ready(function() {
 
     $("#titulo-sitio").html("Presentismo");
     
+    function getUrlParameter(name) {
+        name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+        var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+        var results = regex.exec(location.search);
+        return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+    };
+    
 })
 
 
