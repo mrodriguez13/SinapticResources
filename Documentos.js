6251686c19bff5f2e74368b8c2bd5d3b3480a299
Diego Bevilacqua <diegofbevilacqua1998@gmail.com>
2019-04-17 17:49:44 -0300

    Update Documentos.js

--- Documentos.js
+++ Documentos.js
@@ -34,8 +34,9 @@ $(document).ready(function() {
                 break;
                 case 3:
                     $('.documento').addClass('col-md-4');
-		    $('#documento1').css('padding-right','75px');
-		    $('#documento3').css('padding-right','75px');
+		    $('#documento1').css('padding-left','200px');
+		    $('#documento2').css('padding-right','0px');
+		    $('#documento3').css('padding-right','200px');
                 break;
                 case 4:
                     $('.documento').addClass('col-md-3');
